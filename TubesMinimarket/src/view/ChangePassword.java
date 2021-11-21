535e17cd2b5af1d8bea234f13027ef68576793c4
Nicholas Russel <Nicholas Russel@LAPTOP-E1C6PSRA>
2021-11-21 14:36:06 +0700

    tambah change password

--- TubesMinimarket/src/view/ChangePassword.java
+++ TubesMinimarket/src/view/ChangePassword.java
@@ -42,27 +42,27 @@ public class ChangePassword {
         f.setLocationRelativeTo(null);
 
         title = new JLabel("Change Password Form");
-        title.setFont(new Font("Arial", Font.PLAIN, 30));
-        title.setSize(300, 30);
-        title.setLocation(150, 30);
+        title.setFont(new Font("Arial", Font.PLAIN, 20));
+        title.setSize(300, 15);
+        title.setLocation(50, 30);
         f.add(title);
 
         password = new JLabel("Password");
         password.setFont(new Font("Arial", Font.PLAIN, 15));
         password.setSize(300, 20);
-        password.setLocation(50, 50);
+        password.setLocation(50, 100);
         f.add(password);
 
         tpassword = new JPasswordField();
         tpassword.setFont(new Font("Arial", Font.PLAIN, 15));
         tpassword.setSize(200, 20);
-        tpassword.setLocation(50, 120);
+        tpassword.setLocation(50, 150);
         f.add(tpassword);
 
-        confirm = new JButton("Change");
+        confirm = new JButton("Confirm");
         confirm.setFont(new Font("Arial", Font.PLAIN, 15));
         confirm.setSize(100, 20);
-        confirm.setLocation(100, 320);
+        confirm.setLocation(50, 320);
         confirm.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -74,15 +74,15 @@ public class ChangePassword {
                 f2.setLocationRelativeTo(null);
 
                 Controller c = new Controller();
-                String cekPasswordDariMethod = c.getSelectedPassword(orang.getId_person());
+                String cekPasswordDariMethod = c.getSelectedPassword(orang.getId_person()).substring(0,20);
                 String pass = c.getMD5(String.valueOf(tpassword.getPassword()));
 
-                String olahPassword = pass.substring(0, 30);
+                String olahPassword = pass.substring(0, 20);
 
                 if (olahPassword.equals(cekPasswordDariMethod)) {
                     JOptionPane.showMessageDialog(null, "cek password berhasil!");
 
-                    password = new JLabel("Password");
+                    password = new JLabel("New Password");
                     password.setFont(new Font("Arial", Font.PLAIN, 15));
                     password.setSize(300, 20);
                     password.setLocation(50, 50);
@@ -91,19 +91,19 @@ public class ChangePassword {
                     tpassword = new JPasswordField();
                     tpassword.setFont(new Font("Arial", Font.PLAIN, 15));
                     tpassword.setSize(200, 20);
-                    tpassword.setLocation(50, 120);
+                    tpassword.setLocation(50, 100);
                     f2.add(tpassword);
 
-                    retype = new JLabel("New Password");
+                    retype = new JLabel("Retype");
                     retype.setFont(new Font("Arial", Font.PLAIN, 15));
                     retype.setSize(300, 20);
-                    retype.setLocation(50, 190);
+                    retype.setLocation(50, 150);
                     f2.add(retype);
 
                     tretype = new JPasswordField();
                     tretype.setFont(new Font("Arial", Font.PLAIN, 15));
                     tretype.setSize(200, 20);
-                    tretype.setLocation(50, 260);
+                    tretype.setLocation(50, 200);
                     f2.add(tretype);
 
                     change = new JButton("Change");
@@ -113,8 +113,22 @@ public class ChangePassword {
                     change.addActionListener(new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
-                            String newPassword = c.getMD5(String.valueOf(tpassword.getPassword()));
+                            String oldPass = String.valueOf(tpassword.getPassword());
+                            String retypePass = String.valueOf(tretype.getPassword());
+                            
+                            if(oldPass.equals(retypePass)){
+                                String hashedPass = c.getMD5(String.valueOf(tpassword.getPassword()));
+                                boolean updatePass = c.updatePassword(hashedPass, orang.getId_person());
+                                if(updatePass){
+                                    JOptionPane.showMessageDialog(null, "update password berhasil!");
+                                } else {
+                                    JOptionPane.showMessageDialog(null, "update password gagal!");
+                                }
+                            } else {
+                                JOptionPane.showMessageDialog(null, "password dan retype tidak sama!");
+                            }
                             new MainMenuKasir();
+                            f2.setVisible(false);
                         }
                     });
                     f2.add(change);
@@ -122,7 +136,7 @@ public class ChangePassword {
                     cancel = new JButton("Cancel");
                     cancel.setFont(new Font("Arial", Font.PLAIN, 15));
                     cancel.setSize(100, 20);
-                    cancel.setLocation(150, 320);
+                    cancel.setLocation(160, 320);
                     cancel.addActionListener(new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
@@ -131,7 +145,7 @@ public class ChangePassword {
                         }
                     });
                     f2.add(cancel);
-
+                    f2.setVisible(true);
                 } else {
                     JOptionPane.showMessageDialog(null, "Password Salah");
 
@@ -144,7 +158,7 @@ public class ChangePassword {
         cancel = new JButton("Cancel");
         cancel.setFont(new Font("Arial", Font.PLAIN, 15));
         cancel.setSize(100, 20);
-        cancel.setLocation(150, 320);
+        cancel.setLocation(160, 320);
         cancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
