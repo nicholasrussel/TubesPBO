e6bbdd21db0134e8d547a8738310a39533b35a2b
HP <HP@DESKTOP-LTBNAVE>
2021-11-21 19:47:04 +0700

    membenarkanYangTerhapus

--- TubesMinimarket/src/main/Main.java
+++ TubesMinimarket/src/main/Main.java
@@ -17,11 +17,10 @@ public class Main {
     public static void main(String[] args) {
 //   new view.Login();
  //new view.BayarGaji();
-//new view.MainMenuAdmin();
+new view.MainMenuAdmin();
 //new view.UpdateDataKasir();
 //new view.LihatKehadiran();
 //       new Main();
-new view.Register();
        
     }
 }
