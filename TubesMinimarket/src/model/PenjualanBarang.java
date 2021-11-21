3e886223a05343a46886c45c579c9ec5298be601
HP <HP@DESKTOP-LTBNAVE>
2021-11-21 16:00:55 +0700

    membenarkanYangTerhapus

--- TubesMinimarket/src/model/PenjualanBarang.java
+++ TubesMinimarket/src/model/PenjualanBarang.java
@@ -9,6 +9,47 @@ package model;
  *
  * @author HP
  */
-public class PenjualanBarang {
+    public class PenjualanBarang {
+    private String nomorFaktur;
+    private double totalPenjualan;
+    private String jenisPembayaran;
+
+    public PenjualanBarang() {
+    }
+
+    public PenjualanBarang(String nomorFaktur, double totalPenjualan, String jenisPembayaran) {
+        this.nomorFaktur = nomorFaktur;
+        this.totalPenjualan = totalPenjualan;
+        this.jenisPembayaran = jenisPembayaran;
+    }
+
+    public String getNomorFaktur() {
+        return nomorFaktur;
+    }
+
+    public void setNomorFaktur(String nomorFaktur) {
+        this.nomorFaktur = nomorFaktur;
+    }
+
+    public double getTotalPenjualan() {
+        return totalPenjualan;
+    }
+
+    public void setTotalPenjualan(double totalPenjualan) {
+        this.totalPenjualan = totalPenjualan;
+    }
+
+    public String getJenisPembayaran() {
+        return jenisPembayaran;
+    }
+
+    public void setJenisPembayaran(String jenisPembayaran) {
+        this.jenisPembayaran = jenisPembayaran;
+    }
+
+    @Override
+    public String toString() {
+        return "penjualanBarang{" + "nomorFaktur=" + nomorFaktur + ", totalPenjualan=" + totalPenjualan + ", jenisPembayaran=" + jenisPembayaran + '}';
+    }
     
 }
